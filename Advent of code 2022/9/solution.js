2778c31328ee5c9762088ba6349585a787e3adee
Stefan van Spanje <stefan@digitalchefs.nl>
2022-12-10 11:20:30 +0100

    rough setup

--- Advent of code 2022/9/solution.js
+++ Advent of code 2022/9/solution.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
 
-const handleFile = (filename) => {
-  const input = fs.readFileSync(filename, 'utf-8');
-  const lines = input.split('\n');
-  console.log(lines);
+let headPosition = [0,0];
+let tailPosition = [0,0];
+const visitedPositionsForTail = [tailPosition];
+
+const takeStep = (direction) => {
+  // update the headPosition
+  // when needed
+    // update the tailPosition
+    // update the visitedPositionsForTail
 };
 
-handleFile('test.txt');
+const runForFile = (filename) => {
+  const input = fs.readFileSync(filename, 'utf-8');
+  const commands = input.split('\n');
+
+  // per command
+    // determine direction
+    // determine number of steps (n)
+    // call the takeStep function with the correct direction n times
+
+  // make a new const uniqueVisitedPositionsForTail
+    // assign only unique values of visitedPositionsForTail
+  // console log the length of uniqueVisitedPositionsForTail
+};
+
+runForFile('test.txt');
