1267ddc04d19e3e8bfe682bf81d9b50443251f15
Leonie Nijenmanting <nijenmanting@gmail.com>
2023-02-11 12:17:27 +0100

    Part of solution 2022 day 9

--- Advent of code 2022/9/solution.js
+++ Advent of code 2022/9/solution.js
@@ -8,23 +8,56 @@ const getUniqueArraysInsideArray = (arrayOfArrays) => {
 
 const runForFile = (filename) => {
   const input = fs.readFileSync(filename, 'utf-8');
-  const commands = input.split('\n');
+  const lines = input.split('\n');
 
+  console.log(lines);
+  
+  // [0] is vertical, [1] is horizontal axis.
   let headPosition = [0,0];
   let tailPosition = [0,0];
   const visitedPositionsForTail = [[...tailPosition]];
 
   const takeStep = (direction) => {
-    // update the headPosition
+    if (direction === 'R') {
+      headPosition = [headPosition[0],headPosition[1]+1];
+      console.log(headPosition);
+    }
+    if (direction === 'L') {
+      headPosition = [headPosition[0],headPosition[1]-1];
+      console.log(headPosition);
+    }
+    if (direction === 'U') {
+      headPosition = [headPosition[0]+1,headPosition[1]];
+      console.log(headPosition);
+    }
+    if (direction === 'D') {
+      headPosition = [headPosition[0]-1,headPosition[1]];
+      console.log(headPosition);
+    }
+  
+
     // when needed
       // update the tailPosition
       // update the visitedPositionsForTail
   };
-
+takeStep('D');
   // per command
     // determine direction
     // determine number of steps (n)
     // call the takeStep function with the correct direction n times
+
+
+
+
+
+
+
+
+    for (let i = 0; i < lines.length; i++) {
+      const [direction, n] = lines[i].split(' ');
+
+
+    }
 
   const uniqueVisitedPositionsForTail = getUniqueArraysInsideArray(visitedPositionsForTail);
 
